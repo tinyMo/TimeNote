9e73f3cf71f82e1e03ed4f7c9b262cde254fc19b
tinyMo <baicai003@163.com>
2014-11-06 14:58:24 +0800

    第一次提交

---
+++ TimeNote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimeNote
+//
+//  Created by 楼 彬 on 14-11-6.
+//  Copyright (c) 2014年 lb1006. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TimeNote/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TimeNote
+//
+//  Created by 楼 彬 on 14-11-6.
+//  Copyright (c) 2014年 lb1006. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
